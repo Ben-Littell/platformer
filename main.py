ed2cf670c2e4721f2ea62c9a6be1489e3e6803ec
Ben-Littell <89919066+Ben-Littell@users.noreply.github.com>
2022-01-05 09:21:55 -0500

    get from png

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import math
 import random
 from settings import *
+from sprites import SpriteSheet
 
 pygame.init()
 
@@ -17,7 +18,7 @@ COLORS = [RED, GREEN, BLUE, BLACK]
 PI = math.pi
 
 # Game Constants
-SIZE = (WIDTH, HEIGHT)
+SIZE = (WIN_WIDTH, WIN_HEIGHT)
 FPS = 60
 
 ##############################################################################
@@ -27,6 +28,19 @@ pygame.init()
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Animation Intro')
+bg_image = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+bg_image = pygame.transform.scale(bg_image, (WIN_WIDTH, WIN_HEIGHT))
+
+card = SpriteSheet('assets/deck_of_cards.png')
+x_margin = 11
+y_margin = 2
+x_pad = 22
+y_pad = 4
+
+# card_list = card.load_grid_images(4, 14, x_margin, x_pad, y_margin, y_pad)
+# print(card_list)
+ace_hearts = card.image_at((11, 2, 43, 59))
+
 
 clock = pygame.time.Clock()
 
@@ -42,43 +56,18 @@ while running:
             running = False
 
     screen.fill(BLUE)
-    for i in range(1, WIDTH // tile_size[0]):
-        pygame.draw.rect(screen, WHITE, (i*tile_size[0], 0, 3, HEIGHT))
+    for i in range(1, WIN_WIDTH // tile_size):
+        pygame.draw.rect(screen, WHITE, (i*tile_size, 0, 3, WIN_HEIGHT))
 
+    for j in range(1, WIN_HEIGHT // tile_size):
+        pygame.draw.rect(screen, WHITE, (0, j*tile_size, WIN_WIDTH, 3))
 
+    screen.blit(ace_hearts, (100, 100))
     pygame.display.flip()
 
     clock.tick(FPS)
 
 # outside of game loop
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- settings.py
+++ settings.py
@@ -1,4 +1,15 @@
-WIDTH = 1000
-HEIGHT = 700
-tile_size = (20, 20)
+# WIDTH = 1000
+# HEIGHT = 700
+# tile_size = (50, 50)
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (242, 235, 31)
+GREEN = (31, 242, 137)
+RED = (255, 0, 0)
+
+FPS = 60
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+tile_size = 50
